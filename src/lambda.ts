39b58af928ae9c5857bae1d37a09c8490ba7a0d8
Promie Yutasane <promie.yutasane@gmail.com>
2024-05-24 15:35:32 +1000

    T4G-05 github actions config

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,16 +1,16 @@
-import { configure as serverlessExpress } from '@vendia/serverless-express';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from "./app.module";
+import { configure as serverlessExpress } from '@vendia/serverless-express'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
-let cachedServer;
+let cachedServer
 
 export const handler = async (event, context) => {
   if (!cachedServer) {
-    const nestApp = await NestFactory.create(AppModule);
-    await nestApp.init();
+    const nestApp = await NestFactory.create(AppModule)
+    await nestApp.init()
     cachedServer = serverlessExpress({
-      app: nestApp.getHttpAdapter().getInstance()
-    });
+      app: nestApp.getHttpAdapter().getInstance(),
+    })
   }
-  return cachedServer(event, context);
+  return cachedServer(event, context)
 }
