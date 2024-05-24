7307cf2f038025fc876093a6986c5b618456a304
Promie Yutasane <promie.yutasane@gmail.com>
2024-05-25 07:26:55 +1000

    T4G-07 defined typeorm and user modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { HealthController } from './health/health.controller'
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      username: process.env.PG_USER,
+      password: process.env.PG_PASS,
+      database: process.env.PG_DATABASE,
+      ssl: true,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [HealthController],
   providers: [],
 })
