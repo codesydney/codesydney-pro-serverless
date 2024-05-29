f29967a3535ee6fcdadd1e237d632137d6e4189c
Promie Yutasane <promie.yutasane@gmail.com>
2024-05-29 11:38:08 +1000

    enable CORS on nest and lambda

--- src/lambda.ts
+++ src/lambda.ts
@@ -7,6 +7,14 @@ let cachedServer
 export const handler = async (event, context) => {
   if (!cachedServer) {
     const nestApp = await NestFactory.create(AppModule)
+
+    // Enable CORS
+    nestApp.enableCors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+    })
+
     await nestApp.init()
     cachedServer = serverlessExpress({
       app: nestApp.getHttpAdapter().getInstance(),
